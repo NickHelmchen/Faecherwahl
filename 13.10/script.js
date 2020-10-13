655f241e3dd64208eba7575902760d49ebecc8a8
NickHelmchen <64381231+NickHelmchen@users.noreply.github.com>
2020-10-13 15:56:24 +0200

    Create script.js

---
+++ 13.10/script.js
@@ -0,0 +1,25 @@
+anzeigen = toggle-buttons
+nichtanzeigen = toggle-buttons-invisible
+
+function url() {
+  let url = window.location.href
+  let params = (new URL(url).searchParams)
+
+  document.getElementById("name").value = params.get('name')
+  document.getElementById("lastname").value = params.get('lastname')
+}
+
+function gesellschaftlich() {
+  if(document.getElementById("pfach1.3").checked == true) {
+    document.getElementById("pfach1.3").checked = false
+  }
+  else{
+    document.getElementById("pfach1.3").checked = true
+  }
+  if(document.getElementById("pfach3.4").checked == true) {
+    document.getElementById("pfach3.4").checked = false
+  }
+  else{
+  document.getElementById("pfach3.4").checked = true
+  }
+}
