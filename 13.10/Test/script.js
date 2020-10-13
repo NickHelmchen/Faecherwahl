f2421965a02a5486aebfb47f90c2dbfe9e27b6ea
NickHelmchen <64381231+NickHelmchen@users.noreply.github.com>
2020-10-13 16:34:42 +0200

    Create script.js

---
+++ 13.10/Test/script.js
@@ -0,0 +1,38 @@
+let url = window.location.href
+let params = (new URL(url).searchParams)
+
+function url() { 
+  document.getElementById("name").value = params.get('name')
+  document.getElementById("lastname").value = params.get('lastname')
+
+  if(params.get("Profiel") == "MaNa") {
+    
+  }
+}
+
+function gesellschaftlich() {
+  if(document.getElementById("pfach1.3").checked == true) {
+    document.getElementById("pfach1.3").checked = false
+  }
+  else{
+    document.getElementById("pfach1.3").checked = true
+  }
+  if(document.getElementById("pfach3.4").checked == true) {
+    document.getElementById("pfach3.4").checked = false
+  }
+  else{
+  document.getElementById("pfach3.4").checked = true
+  }
+}
+
+function natur() {
+
+}
+
+function muku() {
+
+}
+
+function sprac() {
+
+}
