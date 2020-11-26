12e32e12557ed7b13adbc6569564525576b97d77
NickHelmchen <64381231+NickHelmchen@users.noreply.github.com>
2020-11-26 12:46:10 +0100

    Update output.js

--- output.js
+++ output.js
@@ -16,3 +16,13 @@ function output() {
     }
   }
 
+function Divprint() {
+    var printContents = document.getElementById("printArea").innerHTML
+    var originalContents = document.body.innerHTML
+
+    document.body.innerHTML = printContents
+
+    window.print()
+
+    document.body.innerHTML = originalContents
+}
