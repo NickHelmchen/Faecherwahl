7eb8e9d8560d1592a9cc99ca613617ae3195fa5a
domephant <66464989+domephant@users.noreply.github.com>
2021-03-08 07:46:56 +0100

    test

--- php/php/wahl/test.php
+++ php/php/wahl/test.php
@@ -6,5 +6,22 @@ function change(){
 ?>
 <html>
 <body onload="<?php change() ?>">
+<form action="test.php" method="post">
+    <input type="radio" method="post" name="pfach1" id="pfach1.1" value="Mathe" onclick="<?php $_SESSION['error'] = false; ?>">
+    <label for="pfach1.1" title="Kernfach, Bereich C" id="pfach1.1.label">Mathe</label>
+    <br>
+    <br>
+    <input type="radio" method="post" name="pfach1" id="pfach1.2" value="Chemie" onclick="">
+    <label for="pfach1.2" title="kein Kernfach, Bereich C" id="pfach1.2.label">Chemie</label>
+    <br>
+    <br>
+    <?php
+        if (isset($_POST['error'])) {
+            if (isset($_POST['pfach1'])) {
+                echo $_POST['pfach1'];
+            }
+        } 
+    ?>
+    </form>
 </body>
 </html>
